6945bf8dab8a47cccd23542f13810f545b00dd15
unknown <tonycruz417@gmail.com>
2020-05-15 06:25:45 -0500

    working on routes

--- client/src/components/Frontpage.js
+++ client/src/components/Frontpage.js
@@ -1,10 +1,26 @@
 import React from 'react';
+import {
+    Collapse,
+    Navbar,
+    NavbarToggler,
+    NavbarBrand,
+    Nav,
+    Container,
+    Row,
+    Col,
+    Button
+} from 'reactstrap';
+import oldSchool from '../images/oldSchool.png';
 
 const displayFront = () => {
     return(
-        <div>
-
-        </div>
+        <section className="hero-image">
+            <div>
+                <Navbar dark expand='sm'>
+                    <NavbarBrand>RuneGuide</NavbarBrand>
+                </Navbar>
+            </div>
+        </section>
     )
 }
 
