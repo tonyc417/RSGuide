3ea1a275e2f966b40566a549ba9f634b9606a5ce
unknown <tonycruz417@gmail.com>
2020-05-24 12:16:29 -0500

    working on cards

--- client/src/components/Guides.js
+++ client/src/components/Guides.js
@@ -3,7 +3,11 @@ import {
     Container, Card, CardImg, CardText, CardBody,
     CardTitle, CardSubtitle, Button, Row, Col
 } from 'reactstrap';
-import CombatIcon from '../images/Attack_icon.png'
+import CombatIcon from '../images/Attack_icon.png';
+import Prayer from '../images/Prayer.png';
+import Ranged from '../images/Ranged.png';
+import Magic from '../images/Magic.png';
+import Woodcutting from '../images/Woodcutting.png';
 
 
 const Guide = () => {
@@ -21,16 +25,16 @@ const Guide = () => {
             </Col>
             <Col>
             <Card className="customCard">
-                <CardImg  width="4%" src={CombatIcon} alt="Card image cap" />
+                <CardImg  width="4%" src={Ranged} alt="Card image cap" />
                 <CardBody>
                     <CardTitle>Ranged</CardTitle>
-                    <Button>Button</Button>
+                    <Button>Go</Button>
                 </CardBody>
             </Card>
             </Col>
             <Col>
             <Card className="customCard">
-                <CardImg  width="4%" src={CombatIcon} alt="Card image cap" />
+                <CardImg  width="4%" src={Prayer} alt="Card image cap" />
                 <CardBody>
                     <CardTitle>Prayer</CardTitle>
                     <Button>Button</Button>
@@ -39,7 +43,7 @@ const Guide = () => {
             </Col>
             <Col>
             <Card className="customCard">
-                <CardImg  width="4%" src={CombatIcon} alt="Card image cap" />
+                <CardImg  width="4%" src={Magic} alt="Card image cap" />
                 <CardBody>
                     <CardTitle>Magic</CardTitle>
                     <Button>Button</Button>
@@ -48,7 +52,7 @@ const Guide = () => {
             </Col>
             <Col>
             <Card className="customCard">
-                <CardImg  width="4%" src={CombatIcon} alt="Card image cap" />
+                <CardImg  width="4%" src={Woodcutting} alt="Card image cap" />
                 <CardBody>
                     <CardTitle>Woodcutting</CardTitle>
                     <Button>Button</Button>
