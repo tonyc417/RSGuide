76d35dda60cf75ab7e94de6392d7fbc374c80a7b
unknown <tonycruz417@gmail.com>
2020-05-26 12:44:30 -0500

    working on links

--- client/src/components/Frontpage.js
+++ client/src/components/Frontpage.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import { Row, Col, Button } from 'reactstrap';
+import { Link } from 'react-router-dom';
 
 
 
@@ -7,6 +9,26 @@ class Front extends Component {
         return (
             <div>
                 <section class="hero-image">
+                    <div className="hero-text">
+                        <h1>Your One Stop for anything About Old School Runescape</h1>
+                        <Row>
+                            <Col>
+                                <Link to="/guides">
+                                    <Button>Guides</Button>
+                                </Link>
+                            </Col>
+                            <Col>
+                                <Link to="/money" >
+                                <Button>Money Making</Button>
+                                </Link>
+                            </Col>
+                            <Col>
+                                <Link to="/news" >
+                                <Button>News</Button>
+                                </Link>
+                            </Col>
+                        </Row>
+                    </div>
                 </section>
             </div>
         )
